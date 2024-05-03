8447629a04068eb36be940ba2ac00c2cb5c0dd94
Uzuko <38263@mail.ru>
2024-05-03 23:49:37 +0300

    initial commit

---
+++ NewsPaper/some_models/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Order(models.Model):  # наследуемся от класса Model
+    pass
+
+class Product(models.Model):
+    pass
+
+class Staff(models.Model):
+    pass
+
+class ProductOrder(models.Model):
+    pass
